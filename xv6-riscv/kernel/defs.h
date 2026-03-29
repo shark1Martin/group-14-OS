a0be2ee7152f999b28f0e0af0b799b13a5c30fe3
hassdbouk <hdbouk@uwo.ca>
2026-03-29 19:29:36 -0400

    contradicting function declarations from other files, its supposed to return an int

--- xv6-riscv/kernel/defs.h
+++ xv6-riscv/kernel/defs.h
@@ -105,7 +105,7 @@ int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
 // check_deadlock declaration
-void            kcheck_deadlock(void);
+int            kcheck_deadlock(void);
 
 // swtch.S
 void            swtch(struct context*, struct context*);
